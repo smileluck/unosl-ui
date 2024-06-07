12d1d29f530407fc977b2b4441a2e0fa8ced1180
smileluck <12386904+smileluck@users.noreply.github.com>
2024-06-07 11:07:05 +0800

    feat: add playground packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-import babelParse from "@babel/eslint-parser"
 
 
 export default [
@@ -26,7 +25,6 @@ export default [
   {
     languageOptions: {
       globals: globals.browser,
-      parser: tseslint.parser,
       parserOptions: {
         sourceType: 'module',
         parser: tseslint.parser,
